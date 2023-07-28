e955fd8a77ff7288e7dd3718bac46daaa935180a
almas <almasyaseen18@gmail.com>
2023-07-28 15:13:55 +0530

    Vendor changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from .models import User
+from .models import User,Userprofile
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     confirm_password =forms.CharField(widget=forms.PasswordInput())
@@ -17,3 +17,10 @@ class UserForm(forms.ModelForm):
                 "password does not match"
                 
             )
+class UserProfileForm(forms.ModelForm):
+    profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+    cover_photo = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+    class Meta:
+        model=Userprofile
+        fields =['profile_picture','cover_photo','address_line_1','address_line_2','country','state','city','pin_code','latitude','longitude']
+    
--- vendor/views.py
+++ vendor/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from .forms import VendorForm
+from django.shortcuts import get_object_or_404,render
+from accounts.forms import UserProfileForm
+from accounts.models import Userprofile
+from .models import Vendor
 # Create your views here.
 def vprofile(request):
-    return render(request,'vendor/vprofile.html')
+    profile = get_object_or_404(Userprofile,user=request.user)
+    vendor = get_object_or_404(Vendor,user=request.user)
+    profile_form = UserProfileForm(instance=profile)
+    vendor_form = VendorForm(instance=vendor)
+    context = { 
+               'profile_form':profile_form,
+               'vendor_form':vendor_form,
+               'profile':profile,
+               'vendor':vendor,
+               }
+    
+    return render(request,'vendor/vprofile.html',context)
