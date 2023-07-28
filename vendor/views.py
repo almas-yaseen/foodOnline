35d9a00a7645aa7c2cb266ef67ca7e29cf9706ba
almas <almasyaseen18@gmail.com>
2023-07-28 19:44:09 +0530

    vendor profileform + validation

--- vendor/views.py
+++ vendor/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 from .forms import VendorForm
-from django.shortcuts import get_object_or_404,render
+from django.shortcuts import get_object_or_404,render,redirect
 from accounts.forms import UserProfileForm
 from accounts.models import Userprofile
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required,user_passes_test
 from .models import Vendor
+from accounts.views import check_role_vendor
 # Create your views here.
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def vprofile(request):
     profile = get_object_or_404(Userprofile,user=request.user)
     vendor = get_object_or_404(Vendor,user=request.user)
+    
+    if request.method=="POST":
+        profile_form = UserProfileForm(request.POST,request.FILES,instance=profile)
+        vendor_form  = VendorForm(request.POST,request.FILES,instance=vendor)
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request,'Settings updated')
+            return redirect('vprofile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+            
+        
     profile_form = UserProfileForm(instance=profile)
     vendor_form = VendorForm(instance=vendor)
     context = { 
